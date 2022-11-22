5e70c67090646717a4e247fdefffef20f33c9924
Tristan <t.bonnet27@laposte.net>
2022-11-22 10:27:52 +0100

    [Add] Prefab player and camera manager

--- Book2022Project/Assets/Scripts/Player/ScrapManager.cs
+++ Book2022Project/Assets/Scripts/Player/ScrapManager.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrapManager : MonoBehaviour
-{
-    
+{   
+    private int _scrapNumber = 0;
+
+    public void AddScrap(int scrapAdded)
+    {
+        _scrapNumber += _scrapNumber;
+
+
+
+    }
 
 }
