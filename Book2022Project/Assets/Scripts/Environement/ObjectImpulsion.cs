f8ff73674676907ef5cd253bed560b1567afe5b0
Tristan <t.bonnet27@laposte.net>
2022-11-24 16:35:37 +0100

    [Modify] Scrap 1.prefab

--- Book2022Project/Assets/Scripts/Environement/ObjectImpulsion.cs
+++ Book2022Project/Assets/Scripts/Environement/ObjectImpulsion.cs
@@ -7,11 +7,26 @@ public class ObjectImpulsion : MonoBehaviour
     [SerializeField]
     Rigidbody _rigibody = null;
     [SerializeField]
+    Collider _collider = null;
+    [SerializeField]
     float _maxTimeBeforeDelete = 0.5f;
+    [SerializeField]
+    float _impulsionForce = 10000;
+    
 
     private void Start()
     {
 
-        _rigibody.AddForce(Vector3.up * 1000);
+        
+        
+    }
+
+    private void OnEnable()
+    {
+        _rigibody.AddForce(Vector3.up * _impulsionForce);
+        float ZVelocity = Random.Range(100, 200);
+        float XVelocity =  Random.Range(100, 200);
+        Vector3 XZVelocity = new Vector3(XVelocity, 0, ZVelocity);
+        _rigibody.AddForce(XZVelocity);
     }
 }
