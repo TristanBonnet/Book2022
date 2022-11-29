ebeaba855f56996210cc09915254dfc86489e4e0
Tristan <t.bonnet27@laposte.net>
2022-11-29 13:55:39 +0100

    [Add] Enemies/EnemyUp.cs.meta

---
+++ Book2022Project/Assets/Scripts/Player/StunEnemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunEnemy : MonoBehaviour
+{
+    [SerializeField]
+    PlayerState _playerState = null;
+
+    [SerializeField]
+    Rigidbody _playerRigibody = null;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_playerState._currentState == PlayerState.GeneralState.InAir && _playerState._currentInAirSubState == PlayerState.InAirSubState.Falling)
+        {
+            Enemy enemy = other.GetComponentInParent<Enemy>();
+            EnemyUp enemyUp = other.GetComponentInParent<EnemyUp>();
+            if (enemy != null && enemyUp != null)
+            {
+                Debug.Log("STUN ENEMY");
+                enemy.StunEnemy();
+
+                if (_playerRigibody != null)
+                {
+                    _playerRigibody.AddForce(Vector3.up * 1000);
+                }
+
+            }
+        }
+    }
+}
